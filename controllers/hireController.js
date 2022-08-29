1584a86be4ddea39f2f673b433e83520daa0438f
classyrazy <classyrazy014@gmail.com>
2022-08-29 05:52:10 +0100

    More functionalities added

--- controllers/hireController.js
+++ controllers/hireController.js
@@ -31,7 +31,7 @@ async function createJob(req, res, next) {
     //             }
     //         })
     //     }
-        
+
     // })
     try {
         // Remenber to add multer 
@@ -61,17 +61,78 @@ async function createJob(req, res, next) {
 
 }
 
-async  function getAllcompanyPostedJobs(req, res, next) {
-        try {
-            const Query = `SELECT * FROM ${SCHEMA}.jobs WHERE company_id = "${req.user.id}"`
-            const allcompanyPostedJobs = await db.query(Query)
-            res.status(200).json(allcompanyPostedJobs.data)
-        } catch (error) {
-            res.status(500).json({ error: error.message })
-        }
+async function getAllcompanyPostedJobs(req, res, next) {
+    try {
+        const Query = `SELECT * FROM ${SCHEMA}.jobs WHERE company_id = "${req.user.id}"`
+        const allcompanyPostedJobs = await db.query(Query)
+        res.status(200).json(allcompanyPostedJobs.data)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+async function startReview(req, res, next) {
+    const { skills } = req.body;
+    if (!skills) {
+        return res.status(400).json({ error: "Please enter all fields" })
+    }
+    let testArr = []
+    try {
+
+        db.searchByValue({
+            table: "developer_profile",
+            searchAttribute: "id",
+            searchValue: "*",
+            attributes: ["id", "skills"],
+        }, (err, response) => {
+            if (err) {
+                next(new Error(err.error))
+            }
+            res.status(200).json(response.data)
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+async function createReview(req, res, next) {
+    const { developersArr, skillsArr } = req.body;
+    if (!developersArr || !skillsArr) {
+        console.log("developersArr", developersArr, "skills", skillsArr)
+        return res.status(400).json({ error: "Please enter all fields" })
+    }
+    try {
+        const newReview = await db.insert({
+            table: 'review',
+            records: [{
+                company_id: req.user.id,
+                developer_array: developersArr,
+                status: "pending",
+                skills_selected: skillsArr
+            }],
+        })
+        res.status(200).json(newReview.data)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+
+}
+
+async function getProfile(req, res, next) {
+    const { routeParam, routeQuery} = req.body;
+    try {
+        const Query = `SELECT * FROM ${SCHEMA}.review WHERE id = "${routeParam}"`
+        const review = await db.query(Query)
+        console.log(review.data)
+        res.status(200).json(review.data)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 }
 
 module.exports = {
     createJob,
-    getAllcompanyPostedJobs
+    getAllcompanyPostedJobs,
+    startReview,
+    createReview,
+    getProfile
 }
