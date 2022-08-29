9de45d6b18c6ce4e0bc4f83a84a11e90c51ba6b7
classyrazy <classyrazy014@gmail.com>
2022-08-29 15:54:01 +0100

    fixing access control error

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ var corsOptions = {
         }
     }
 }
-app.use(cors(corsOptions));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/hire", hireRoutes);
