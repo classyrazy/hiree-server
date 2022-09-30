f8f421fee312bce534f28624963ad5cf833a85c7
classyrazy <classyrazy014@gmail.com>
2022-09-30 17:12:32 +0100

    Fix: companyLocation added to reqbody create job

--- controllers/hireController.js
+++ controllers/hireController.js
@@ -5,7 +5,7 @@ const SCHEMA = process.env.INSTANCE_SCHEMA;
 
 async function createJob(req, res, next) {
     console.log(req.body)
-    const { companyName, workModel, title, description, jobLocationType, skills, benefits, } = req.body
+    const { companyName, workModel, title, description, jobLocationType, skills, benefits, companyLocation} = req.body
     if (!companyName || !workModel || !title || !description || !jobLocationType || !skills || benefits || companyLocation ) {
         return res.status(400).json({ error: "Please enter all fields" })
     }
